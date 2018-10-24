187095de1b0ab77def8482354367067b0b767365
XDS <554536080@qq.com>
2018-10-24 17:37:34 +0800

    third - push

--- AndroidStudioProjects/VIVO/AndroidStudioProjects/MultiNotesIdentification/app/src/main/java/com/example/xds/multinotesidentification/MainActivity.java
+++ AndroidStudioProjects/VIVO/AndroidStudioProjects/MultiNotesIdentification/app/src/main/java/com/example/xds/multinotesidentification/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
